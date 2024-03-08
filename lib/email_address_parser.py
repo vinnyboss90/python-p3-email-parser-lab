8ca225c18b996bc8064855b1bc8aa428cff83401
vinnyegon90 <vinnyegon90@gmail.com>
2024-03-08 11:14:56 +0300

    Done

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,12 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+    
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+        pattern = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
+        matches = pattern.findall(self.email_addresses)
+        return sorted(matches)
